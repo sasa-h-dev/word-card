51ebf434306920df6f74d1d4f67c34c58517fce3
sasa <huang123434@163.com>
2022-04-04 16:54:28 +0900

    add process.env.PORT

--- src/server/main.ts
+++ src/server/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useStaticAssets(join(__dirname, '..', '/word-card-client'));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
